67c1b8e82eafcc17d50024e80c8d4414f46f2f8e
Hendrik Schwartke <hendrik@os-s.de>
2025-05-27 13:23:47 +0000

    unnecessary symbols of the plugin hidden

--- plugin.cmake
+++ plugin.cmake
@@ -3,10 +3,14 @@ add_compile_options(-Wextra -Wall -Wno-implicit-fallthrough -Werror)
 
 include_directories("${CMAKE_CURRENT_SOURCE_DIR}/plugin/src/asn1c")
 
-# The following executables are for testing purposes only
-
 file(GLOB asn1c_files RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "plugin/src/asn1c/*.c")
 add_library(asn1c OBJECT ${asn1c_files})
+set_target_properties(asn1c PROPERTIES POSITION_INDEPENDENT_CODE ON)
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+    target_compile_options(asn1c PRIVATE -fvisibility=hidden)
+endif()
+
+# The following executables are for testing purposes only
 
 add_executable(test-cp $<TARGET_OBJECTS:asn1c> testing/Files/asn1c-test.c)
 target_compile_definitions(test-cp PRIVATE PDU=CP_type)
